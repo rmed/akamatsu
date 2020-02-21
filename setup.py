6cf3c2564401dc4f45b747674c5a0c2decc9518c
Rafael Medina <rafamedgar@gmail.com>
2020-02-21 20:55:40 +0100

    Added requirements to setup.py

--- setup.py
+++ setup.py
@@ -52,6 +52,35 @@ setup(
 
     zip_safe=False,
 
+    install_requires=[
+        'awesome-slugify==1.6.5',
+        'bcrypt>=3.1.7',
+        'blinker==1.4',
+        'Flask==1.1.1',
+        'Flask-Analytics==0.6.0',
+        'Flask-Assets==2.0',
+        'Flask-Babel==1.0.0',
+        'Flask-Discussion==0.1.0',
+        'Flask-Login==0.5.0',
+        'Flask-Migrate==2.5.2',
+        'Flask-Mail==0.9.1',
+        'Flask-Misaka==1.0.0',
+        'Flask-SQLAlchemy==2.4.1',
+        'Flask-WTF==0.14.3',
+        'hashids==1.2.0',
+        'passlib>=1.7.2',
+        'Pygments>=2.5.2',
+        'Werkzeug>=1.0.0'
+    ],
+
+    extras_require={
+        'dev': [
+            'cssmin==0.2.0',
+            'Flask-DebugToolbar==0.11.0',
+            'libsass==0.19.4'
+        ]
+    },
+
     entry_points={
         'console_scripts': [
             'akamatsu=akamatsu.commands:cli.main',
